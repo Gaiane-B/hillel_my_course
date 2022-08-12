53d5e90b01f8499239eb75e36a56083062edcbe6
Gaiane-B <gajane3891@gmail.com>
2022-08-12 21:38:53 +0200

    first homework

--- first_task.py
+++ first_task.py
@@ -7,5 +7,5 @@ import antigravity
 # return text Hello world!
 import __hello__
 
-# gives error not a chance. It's a joke about delimiting blocks in Python
+# gives error not a chance (It's a joke about delimiting blocks in Python)
 from __future__ import braces

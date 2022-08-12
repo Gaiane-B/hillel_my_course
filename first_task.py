1f37f6fe0b9776b6997fde83f41a0ce7abedb1b9
Gaiane-B <gajane3891@gmail.com>
2022-08-12 20:59:26 +0200

    first homework

---
+++ first_task.py
@@ -0,0 +1,11 @@
+# opens Zen of Python
+import this
+
+# opens funny comic about Python
+import antigravity
+
+# return text Hello world!
+import __hello__
+
+# gives error not a chance. It's a joke about delimiting blocks in Python
+from __future__ import braces

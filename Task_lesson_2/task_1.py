68e2f5d570aebb57fba3cc8b2e3c9ddddcd17c7f
Gaiane-B <gajane3891@gmail.com>
2022-08-16 23:59:14 +0200

    second homework

---
+++ Task_lesson_2/task_1.py
@@ -0,0 +1,10 @@
+# I wrote two options
+# First option
+name = input()
+print("Hello, " + name + "!")
+
+# Second option
+def greeting(user_name):
+    print("Hello, " + user_name + "!")
+
+greeting("Kitty")

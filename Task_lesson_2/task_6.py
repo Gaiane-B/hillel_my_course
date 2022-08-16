c00922184ccc633d573898f544d7a217784ad23e
Gaiane-B <gajane3891@gmail.com>
2022-08-17 00:00:48 +0200

    second homework

--- Task_lesson_2/task_6.py
+++ Task_lesson_2/task_6.py
@@ -1,5 +1,6 @@
 # I wrote two options
 # First option
+
 a = abs(float(input()))
 my_list = range(10, 101, 10)
 if a in my_list:
@@ -8,5 +9,6 @@ else:
     print("No")
 
 # Second option
+
 a = abs(float(input())) in range(10, 101, 10)
 print(a)

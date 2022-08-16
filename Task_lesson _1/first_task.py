6f4ce459b28237e77298d8eb6063623b2c78e223
Gaiane-B <gajane3891@gmail.com>
2022-08-16 23:53:39 +0200

    first homework

--- Task_lesson _1/first_task.py
+++ Task_lesson _1/first_task.py
@@ -4,7 +4,7 @@ import this
 # opens funny comic about Python
 import antigravity
 
-# return text Hello world! (I checked in IDLE)
+# return text Hello world! I checked in IDLE
 import __hello__
 
 # gives error not a chance (It's a joke about delimiting blocks in Python)
